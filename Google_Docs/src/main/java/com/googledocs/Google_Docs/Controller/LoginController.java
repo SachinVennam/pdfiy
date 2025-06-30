2490a7f01dc2de47ae367f3f5466b510742ff636
SachinVennam <155296214+SachinVennam@users.noreply.github.com>
2025-06-30 20:44:30 +0530

    Sachin Added project on 30062025
    Sachin Added project on 30062025

---
+++ Google_Docs/src/main/java/com/googledocs/Google_Docs/Controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.googledocs.Google_Docs.Controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+	@PostMapping("/login")
+	public String login(@RequestParam("test") String login) {
+		return login;
+	}
+
+}

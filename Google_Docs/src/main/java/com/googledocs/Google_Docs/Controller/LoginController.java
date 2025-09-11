0c5c79d470211e5db45b983c294ad5e7c039dcde
SachinVennam <sachinvennam22@gmail.com>
2025-09-11 15:31:45 +0530

    Login API changes

--- Google_Docs/src/main/java/com/googledocs/Google_Docs/Controller/LoginController.java
+++ Google_Docs/src/main/java/com/googledocs/Google_Docs/Controller/LoginController.java
@@ -4,13 +4,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class LoginController {
-	
-	@PostMapping("/login")
-	public String login(@RequestParam("test") String login) {
-		System.out.println("Hello World...");
-		return login;
-	}
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@RestController
+@Tag(name = "LoginController", description = "Login and Change Password endpoints")
+public class LoginController {
+
+	@Operation(summary = "Login API", description = "Validates User name and Password")
+	@PostMapping("/login")
+	public String login(@RequestParam("userName") String userName,@RequestParam("password") String password) {
+		String response=null;
+		try {
+			if(userName.trim().equals("Sachin Vennam") && password.trim().equals("test@123"))
+				response="Logged in Sucessfully....";
+			else
+				response="User Name or Password is incorrect,Please Check and try again..";
+		} catch (Exception e) {
+			System.out.println("error in login Method.."+e.getMessage());
+			e.printStackTrace();
+		}
+		return response;
+	}
 }
